76cb269f804d971c6397ba0a3065fad8aca904cd
Carl <Carl@LAPTOP-ENVY-I76>
2018-06-19 07:50:58 +0200

    more liteDB

---
+++ LiteDBAppl1/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace LiteDBAppl1
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Name { get; set; }
+        public string[] Phones { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
--- LiteDBAppl1/Program.cs
+++ LiteDBAppl1/Program.cs
@@ -1,9 +1,12 @@
 ﻿using LiteDB;
+using System;
+using System.Linq;
 
 namespace LiteDBAppl1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             using (var db = new LiteDatabase(@"CustomerData.db"))
@@ -16,6 +19,46 @@ namespace LiteDBAppl1
                     Address = "Hyderabad"
                 };
                 collection.Insert(author);
+                // Get customer collection
+                var customers = db.GetCollection<Customer>("customers");
+
+                // Create your new customer instance
+                var customer = new Customer
+                {
+                    Name = "Doe",
+                    FirstName = "John",
+                    Phones = new string[] { "8000-0000", "9000-0000" },
+                    IsActive = true
+                };
+
+                var c2 = new Customer
+                {
+                    Name = "Wei",
+                    FirstName = "Carl",
+                    Phones = new string[] { "8000-0001", "9000-0001" },
+                    IsActive = true
+                };
+                // Insert new customer document (Id will be auto-incremented)
+                customers.Insert(customer);
+                customers.Insert(c2);
+
+
+                // Update a document inside a collection
+                customer.FirstName = "Joana";
+                customers.Update(customer);
+
+                // Index document using a document property
+                customers.EnsureIndex(x => x.Name);
+
+                // Use Linq to query documents
+                var results = customers.Find(x => x.Name.StartsWith("Do"));
+                Console.WriteLine(customers.Count());
+                Console.WriteLine(results.Count());
+                Console.WriteLine(results.AsParallel());
+                var foundAuthor = collection.FindById(1);
+                Console.WriteLine(foundAuthor.FirstName + "\t" + foundAuthor.LastName);
+                var FoundMaxCustomer = customers.FindById(customers.Count());
+                Console.WriteLine("Latest Customer= " + FoundMaxCustomer.FirstName);
             }
         }
     }
